8e846cc3cea92c3dc63ca0e6ea5b7957a2955623
eliaka <eliaka@users.noreply.github.com>
2020-12-09 17:57:12 +0100

    Update finetuning_flowers.py

--- finetuning_flowers.py
+++ finetuning_flowers.py
@@ -71,7 +71,7 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_ince
         print('-' * 10)
 
         # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
+        for phase in ['train', 'test']:
             if phase == 'train':
                 model.train()  # Set model to training mode
             else:
